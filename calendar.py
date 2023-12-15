95f3b6e6db81f8bea77ddc7f49073f30ff87ed43
echoi3713 <50953269+echoi3713@users.noreply.github.com>
2023-12-15 02:01:08 -0500

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -10,6 +10,7 @@ class Calendar:
         del self.events[event_name]
     
     def display_events(self):
+        print("--------------------------------")
         print(self.name + ": \n")
         for event in self.events:
             event = self.events[event]
@@ -31,35 +32,112 @@ class Calendar:
             else:
                 print("Recurring: " + event.recurring)
             print()
-        print("---------------------------------------")
+        
     
 
 class Event:
-    def __init__(self, name, date, time=None, location=None, recurring=None):
-        self.name = name
-        self.date = date
-        
-        if (time!=None and time<1000):
-            time_to_string = str(time)
-            self.time = "0" + time_to_string
+    def __init__(self, details):
+        details = details.split(', ')
+
+        self.name = details[0]
+        self.date = details[1]
+
+        if details[2] != "None":
+            print(details[2]+"a")
+            time = details[2]
+            if (int(time)<1000):
+                self.time = "0" + time
         else:
-            self.time = time
+            self.time = details[2]
         
-        self.location = location
-        self.recurring = recurring
-    
+        self.location = details[3]
+        
+        self.recurring = details[4]
+
+
+def get_user_input():
+
+    while True:
+
+        name = input("Enter calendar name: ")
+
+        if name:
+
+            return Calendar(name)
+
+        else:
+
+            print("Invalid input. Please enter a name for the calendar.")
+
 if __name__ == "__main__":
-        calendar1 = Calendar("To Do")
-        calendar1.add_event(Event("Club Meeting", "12/11/2023", 1600, "Tawes"))
-        calendar1.add_event(Event("Work", "12/11/2023", 900, "Work", "daily"))
-        calendar1.add_event(Event("Homework", "12/13/2023", 1900, None, "weekly"))
-        calendar1.display_events()
-        calendar1.delete_event("Homework")
-        print("\nAFTER DELETION:\n")
-        calendar1.display_events()
-        
-        calendar2 = Calendar("Final Exams")
-        calendar2.add_event(Event("Math Exam", "12/18/2023", 800, "Math building"))
-        calendar2.add_event(Event("Physics Exam", "12/18/2023", 1330, "Physics building"))
-        calendar2.add_event(Event("History Exam", "12/19/2023", 1600, "Online"))
-        calendar2.display_events()
+    calendars = []
+
+    while True:
+        print("================================")
+        print("1. Create Calendar")
+        print("2. Add Event")
+        print("3. Delete Event")
+        print("4. Display Events")
+        print("5. Exit")
+
+        choice = input("Enter your choice (1-5): ")
+
+        if choice == '1':
+            calendars.append(get_user_input())
+
+        elif choice == '2':
+            if not calendars:
+                print("Please create a calendar first.")
+
+            else:
+                calendar_name = input("Enter calendar name to add event: ")
+                calendar = next((cal for cal in calendars if cal.name == calendar_name), None)
+
+                if calendar:
+                    event = Event(input("Enter event details (name, date, time(None), location(None), recurring(None) ): "))
+                    calendar.add_event(event)
+                    print("Event added successfully.")
+
+                else:
+
+                    print(f"Calendar '{calendar_name}' not found.")
+
+        elif choice == '3':
+            if not calendars:
+                print("Please create a calendar first.")
+
+            else:
+                calendar_name = input("Enter calendar name to delete event: ")
+                calendar = next((cal for cal in calendars if cal.name == calendar_name), None)
+
+                if calendar:
+                    event_name = input("Enter event name to delete: ")
+                    calendar.delete_event(event_name)
+
+                else:
+                    print(f"Calendar '{calendar_name}' not found.")
+
+        elif choice == '4':
+            if not calendars:
+                print("Please create a calendar first.")
+
+            else:
+                calendar_name = input("Enter calendar name to display events: ")
+                calendar = next((cal for cal in calendars if cal.name == calendar_name), None)
+
+                if calendar:
+                    if calendar.events:
+                        for calendar in calendars:
+                            calendar.display_events()
+                    else: 
+                        print("No events to display")
+
+                else: 
+                    print(f"Calendar '{calendar_name}' not found.")
+
+        elif choice == '5':
+            print("Exiting the program. Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please enter a number between 1 and 5.")
